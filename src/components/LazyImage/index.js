1f70b4beb31a12b06db96ef63d318ba9e2784f8d
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-31 15:09:50 -0300

    colocando o Animated no component Original

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Animated } from "react-native";
 import { View } from "react-native";
 
 import { Small, Original } from "./styles";
 
+const OriginalAnimated = Animated.createAnimatedComponent(Original);
+
 export default function LazyImage({ smallSource, source, aspectRatio }) {
+  const opacity = new Animated.Value(0);
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+  }, []);
+
+  function handleAnimate() {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  }
+
   return (
     <Small
       source={smallSource}
@@ -11,7 +32,15 @@ export default function LazyImage({ smallSource, source, aspectRatio }) {
       resizeMode="contain"
       blurRadius={2}
     >
-      <Original source={source} ratio={aspectRatio} resizeMode="contain" />
+      {loaded && (
+        <OriginalAnimated
+          style={{ opacity }}
+          source={source}
+          ratio={aspectRatio}
+          resizeMode="contain"
+          onLoadEnd={handleAnimate}
+        />
+      )}
     </Small>
   );
 }
