d8badb713b7abc0b196a92d35a388c1f8af8721a
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-01-31 17:59:47 -0300

    adicionando icones na header do app

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { Text, Image } from "react-native";
+import { TouchableOpacity, Text, Image, View } from "react-native";
 
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
 import Feed from "../Screens/Feed";
 
-import logo from "../../assets/instagram.png";
+import camera from "../../assets/camera.png";
+import logo from "../../assets/logo.png";
+import igtv from "../../assets/igtv.png";
+import send from "../../assets/send.png";
 
 const MainNavigator = createStackNavigator(
   { Feed },
   {
     defaultNavigationOptions: {
       headerTitle: () => <Image source={logo} />,
+      headerLeft: () => (
+        <TouchableOpacity style={{ marginLeft: 20 }}>
+          <Image source={camera} />
+        </TouchableOpacity>
+      ),
+      headerRight: () => (
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity style={{ marginRight: 20 }}>
+            <Image source={igtv} />
+          </TouchableOpacity>
+          <TouchableOpacity style={{ marginRight: 20 }}>
+            <Image source={send} />
+          </TouchableOpacity>
+        </View>
+      ),
       headerStyle: {
         backgroundColor: "#f5f5f5"
       }
